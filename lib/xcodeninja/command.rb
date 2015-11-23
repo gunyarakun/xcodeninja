6925ca923be7ec0b3cd283f9fd61a7c9de3fd1a8
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2015-11-22 21:38:13 -0800

    Implement ninja.build output

--- lib/xcodeninja/command.rb
+++ lib/xcodeninja/command.rb
@@ -4,7 +4,7 @@ module XcodeNinja
   require 'claide'
   require 'pp' # for debug
 
-  REFERENCE_FRAMEWORKS = %w|UIKit Security ImageIO GoogleMobileAds CoreGraphics|
+  REFERENCE_FRAMEWORKS = %w(UIKit Security ImageIO GoogleMobileAds CoreGraphics)
 
   class Command < CLAide::Command
     self.command = 'xcodeninja'
@@ -24,42 +24,92 @@ module XcodeNinja
     end
 
     def run
-      ninja_targets = {}
-      xcodeproj.targets.each {|target|
+      unless @xcodeproj_path
+        fail Informative, 'Please specify Xcode project.'
+      end
+
+      header_dirs = xcodeproj.main_group.recursive_children.select { |g| g.path && File.extname(g.path) == '.h' }.map{ |g|
+        full_path = File.join((g.parents + [g]).map{ |group| group.path }.select{ |path| path })
+        File.dirname(full_path)
+      }.to_a.uniq
+
+      xcodeproj.targets.each do |target|
         build_configurations = target.build_configurations
-        build_configurations.each {|bc|
+        build_configurations.each do |bc|
           bs = bc.build_settings
-          lib_dirs = expand(bs['LIBRARY_SEARCH_PATHS'])
-          framework_dirs = expand(bs['FRAMEWORK_SEARCH_PATHS'])
-          target_header_dirs = expand(bs['HEADER_SEARCH_PATHS'])
+          lib_dirs = expand(bs['LIBRARY_SEARCH_PATHS'], :array)
+          framework_dirs = expand(bs['FRAMEWORK_SEARCH_PATHS'], :array)
+          target_header_dirs = expand(bs['HEADER_SEARCH_PATHS'], :array)
 
           lib_options = lib_dirs.map { |dir| "-L#{dir}" }.join(' ')
           framework_options = (framework_dirs.map { |f| "-F#{f}" } + REFERENCE_FRAMEWORKS.map{ |f| "-framework #{f}" }).join(' ')
           header_options = (header_dirs + target_header_dirs).map{ |dir| "-I./#{dir}" }.join(' ')
 
-        }
-      }
-      unless @xcodeproj_path
-        fail Informative, 'Please specify Xcode project.'
-      end
-
-      target.build_phases.each{|phase|
-        case phase
-        when Xcodeproj::Project::Object::PBXResourceBuildPhase
-          resource_build_phase(phase)
-        when Xcodeproj::Project::Object::PBXSourceBuildPhase
-          source_build_phase(phase)
-        else
-          fail Informative, "Don't support the phase #{phase.class.name}."
         end
-      }
+
+        builds = target.build_phases.map do |phase|
+          case phase
+          when Xcodeproj::Project::Object::PBXResourcesBuildPhase
+            resources_build_phase(target, phase)
+          when Xcodeproj::Project::Object::PBXSourcesBuildPhase
+            sources_build_phase(target, phase)
+          when Xcodeproj::Project::Object::PBXFrameworksBuildPhase
+            frameworks_build_phase(target, phase)
+          when Xcodeproj::Project::Object::PBXShellScriptBuildPhase
+            shell_script_build_phase(target, phase)
+          else
+            fail Informative, "Don't support the phase #{phase.class.name}."
+          end
+        end.flatten.compact
+
+        File.open("#{target.name}.ninja.build", 'w:UTF-8') do |f|
+          f.puts rules(target)
+          f.puts ''
+          builds.each do |b|
+            f.puts "build #{b[:outputs].join(' ')}: #{b[:rule_name]} #{b[:inputs].join(' ')}"
+            f.puts ''
+          end
+        end
+      end
     end
 
     private
 
-    def resource_build_phase(target, phase)
-      build_dir = xxxx
-      phase.files_references.each {|file|
+    def rules(target)
+      # TODO: extract minimum-deployment-target from xcodeproj
+      r = <<RULES
+rule ibtool
+  command = ibtool --errors --warnings --notices --module #{target.product_name} --target-device iphone --minimum-deployment-target 9.0 --output-format human-readable-text --compilation-directory `dirname ${out}` ${in} && ibtool --errors --warnings --notices --module #{target.product_name} --target-device iphone --minimum-deployment-target 9.0 --output-format human-readable-text --link #{resources_dir(target)} ${out}
+
+rule cp_r
+  command = cp -R ${in} ${out}
+
+rule file_packager
+  command = python ${EMSCRIPTEN}/tools/file_packager.py #{target.name} --preload #{build_dir(target)}@/ > #{build_dir(target)}/ManboData.js
+RULES
+      r
+    end
+
+    def build_dir(target)
+      "build/#{target.name}"
+    end
+
+    def bundle_dir(target)
+      "#{build_dir(target)}/Contents"
+    end
+
+    def resources_dir(target)
+      "#{bundle_dir(target)}/Resources"
+    end
+
+    def data_path(target)
+      "#{build_dir(target)}/#{target.product_name}.dat"
+    end
+
+    def resources_build_phase(target, phase)
+      builds = []
+      resources = []
+      phase.files_references.each do |file|
         case file
         when Xcodeproj::Project::Object::PBXFileReference
           files = [file]
@@ -69,65 +119,103 @@ module XcodeNinja
           fail Informative, "Don't support the file #{file.class.name}."
         end
 
-        files.each{ |file|
+        files.each do |file|
           local_path = File.join(file.parents.map{|group| group.path}.select{|path| path}, file.path)
-          remote_path = File.join(RESOURCES_DIR, file.path)
+          remote_path = File.join(resources_dir(target), file.path)
 
           #p remote_path, local_path
-          if file.path.end_with?(".storyboard")
-              remote_path += "c"
-              desc remote_path
-              file remote_path => local_path do |t|
-                  sh "ibtool --errors --warnings --notices --module #{APP_NAME} --target-device iphone --minimum-deployment-target 9.0 --output-format human-readable-text --compilation-directory tmp/#{File.dirname(local_path)} #{local_path}"
-                  sh "ibtool --errors --warnings --notices --module #{APP_NAME} --target-device iphone --minimum-deployment-target 9.0 --output-format human-readable-text --link #{RESOURCES_DIR} tmp/#{local_path}c"
-              end
+          if File.extname(file.path) == '.storyboard'
+            remote_path += 'c'
+            builds << {
+              :outputs => [remote_path],
+              :rule_name => 'ibtool',
+              :inputs => [local_path],
+            }
+            [remote_path, 'ibtool', [local_path]]
           else
-              dir = File.dirname(remote_path)
-              directory dir
-              desc remote_path
-              file remote_path => [local_path, dir] do |t|
-                  cp_r local_path, remote_path
-              end
+            builds << {
+              :outputs => [remote_path],
+              :rule_name => 'cp_r',
+              :inputs => [local_path],
+            }
           end
 
           resources << remote_path
-        }
-      }
-      infoplist = File.join(BUNDLE_DIR, "Info.plist")
-      resources << infoplist
-      directory File.dirname(BUNDLE_DIR)
-
-      desc "Info.plist"
-      # TODO fix Info.plist path
-      file infoplist => File.join(APP_NAME, "Resources/Info.plist") do |t|
-          cp t.prerequisites[0], t.name
+        end
       end
+      infoplist = File.join(bundle_dir(target), 'Info.plist')
+      resources << infoplist
+
+      builds << {
+        :outputs => [infoplist],
+        :rule_name => 'cp_r',
+        # TODO fix Info.plist path
+        :inputs => ['Resources/Info.plist'],
+      }
+
+      # UIKit bundle
+      # FIXME: split
+      builds << {
+        :outputs => ["#{build_dir(target)}/frameworks/UIKit.framework/"],
+        :rule_name => 'cp_r',
+        :inputs => ["#{ENV['EMSCRIPTEN']}/system/frameworks/UIKit.framework/Resources/"],
+      }
+      resources << "#{build_dir(target)}/frameworks/UIKit.framework/"
+
+      builds << {
+        :outputs => [data_path(target)],
+        :rule_name => 'file_packager',
+        :inputs => resources,
+      }
+
+      builds
     end
 
-    def source_build_phase(phase)
+    def sources_build_phase(target, phase)
+      # FIXME: Implement
+    end
+
+    def frameworks_build_phase(target, phase)
+      # FIXME: Implement
+    end
+
+    def shell_script_build_phase(target, phase)
+      # FIXME: Implement
     end
 
     def expand(value, type=nil)
       # TODO: Expand $(TARGET_NAME) etc.
       if value.kind_of?(Enumerable)
-        value.reject {|v|
+        value.reject do |v|
           v == '$(inherited)'
-        }.map {|v|
+        end.map do |v|
           expand(v)
-        }
+        end
       else
         case type
         when :bool
           value == 'YES'
+        when :array
+          if value.kind_of?(Array)
+            value
+          elsif value.nil?
+            []
+          else
+            [value]
+          end
         else
-          value.gsub(/$\(([A-Za-z0-9_])\)/) {|m|
-            case m[1]
-            when 'PROJECT_DIR'
-              xcodeproj_dir
-            else
-              fail Informative, "Not support for #{m[1]}"
-            end
-          }
+          if value.nil?
+            nil
+          else
+            value.gsub(/$\(([A-Za-z0-9_])\)/) do |m|
+              case m[1]
+              when 'PROJECT_DIR'
+                xcodeproj_dir
+              else
+                fail Informative, "Not support for #{m[1]}"
+              end
+          end
+          end
         end
       end
     end
