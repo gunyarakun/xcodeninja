0b1a1b969559793611e7bcf0fa2cc8821857045e
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2015-11-24 15:07:41 -0800

    Fix rubocop warnings

--- lib/xcodeninja/command.rb
+++ lib/xcodeninja/command.rb
@@ -46,14 +46,14 @@ module XcodeNinja
           end.flatten.compact
 
           File.open("#{target.name}.#{build_config.name}.ninja.build", 'w:UTF-8') do |f|
-            f.puts rules(target)
+            f.puts rules(target, build_config)
             f.puts ''
             builds.each do |b|
               f.puts "build #{b[:outputs].join(' ')}: #{b[:rule_name]} #{b[:inputs].join(' ')}"
               if b[:variables]
-                b[:variables].each {|k, v|
+                b[:variables].each do |k, v|
                   f.puts "  #{k} = #{v}"
-                }
+                end
               end
               f.puts ''
             end
@@ -64,7 +64,7 @@ module XcodeNinja
 
     private
 
-    def rules(target)
+    def rules(target, build_config)
       # TODO: extract minimum-deployment-target from xcodeproj
       r = <<RULES
 rule ibtool_compile
@@ -83,99 +83,98 @@ rule cp_r
   command = cp -r ${in} ${out}
 
 rule file_packager
-  command = python #{ENV['EMSCRIPTEN']}/tools/file_packager.py ${out} --preload #{build_dir(target)}@/ > #{build_dir(target)}/ManboData.js
+  command = python #{ENV['EMSCRIPTEN']}/tools/file_packager.py ${out} --preload #{build_dir(target, build_config)}@/ > #{build_dir(target, build_config)}/ManboData.js
 RULES
       r
     end
 
-    def build_dir(target)
-      "build/#{target.name}"
+    def build_dir(target, build_config)
+      "build/#{target.name}/#{build_config.name}"
     end
 
-    def bundle_dir(target)
-      "#{build_dir(target)}/Contents"
+    def bundle_dir(target, build_config)
+      "#{build_dir(target, build_config)}/Contents"
     end
 
-    def resources_dir(target)
-      "#{bundle_dir(target)}/Resources"
+    def resources_dir(target, build_config)
+      "#{bundle_dir(target, build_config)}/Resources"
     end
 
-    def objects_dir(target)
-      "#{build_dir(target)}/objects"
+    def objects_dir(target, build_config)
+      "#{build_dir(target, build_config)}/objects"
     end
 
-    def data_path(target)
-      "#{build_dir(target)}/#{target.product_name}.dat"
+    def data_path(target, build_config)
+      "#{build_dir(target, build_config)}/#{target.product_name}.dat"
     end
 
-    def binary_path(target)
-      "#{build_dir(target)}/#{target.product_name}.bc"
+    def binary_path(target, build_config)
+      "#{build_dir(target, build_config)}/#{target.product_name}.bc"
     end
 
-    def resources_build_phase(xcodeproj, target, build_config, phase)
+    def resources_build_phase(_xcodeproj, target, build_config, phase)
       builds = []
       resources = []
-      phase.files_references.each do |file|
-        case file
+      phase.files_references.each do |files_ref|
+        case files_ref
         when Xcodeproj::Project::Object::PBXFileReference
-          files = [file]
+          files = [files_ref]
         when Xcodeproj::Project::Object::PBXVariantGroup
-          files = file.files
+          files = files_ref.files
         else
-          fail Informative, "Don't support the file #{file.class.name}."
+          fail Informative, "Don't support the file #{files_ref.class.name}."
         end
 
         files.each do |file|
-          local_path = File.join(file.parents.map{|group| group.path}.select{|path| path}, file.path)
-          remote_path = File.join(resources_dir(target), file.path)
+          local_path = File.join(file.parents.map(&:path).select { |path| path }, file.path)
+          remote_path = File.join(resources_dir(target, build_config), file.path)
 
-          #p remote_path, local_path
           if File.extname(file.path) == '.storyboard'
             remote_path += 'c'
             tmp_path = File.join('tmp', remote_path)
             builds << {
-              :outputs => [tmp_path],
-              :rule_name => 'ibtool_compile',
-              :inputs => [local_path],
+              outputs: [tmp_path],
+              rule_name: 'ibtool_compile',
+              inputs: [local_path],
             }
             builds << {
-              :outputs => [remote_path],
-              :rule_name => 'ibtool_link',
-              :inputs => [tmp_path],
+              outputs: [remote_path],
+              rule_name: 'ibtool_link',
+              inputs: [tmp_path],
             }
           else
             builds << {
-              :outputs => [remote_path],
-              :rule_name => 'cp_r',
-              :inputs => [local_path],
+              outputs: [remote_path],
+              rule_name: 'cp_r',
+              inputs: [local_path],
             }
           end
 
           resources << remote_path
         end
       end
-      infoplist = File.join(bundle_dir(target), 'Info.plist')
+      infoplist = File.join(bundle_dir(target, build_config), 'Info.plist')
       resources << infoplist
 
       builds << {
-        :outputs => [infoplist],
-        :rule_name => 'cp_r',
-        # TODO fix Info.plist path
-        :inputs => [File.join(target.product_name, 'Resources/Info.plist')],
+        outputs: [infoplist],
+        rule_name: 'cp_r',
+        # TODO: fix Info.plist path
+        inputs: [File.join(target.product_name, 'Resources/Info.plist')],
       }
 
       # UIKit bundle
       builds << {
-        :outputs => ["#{build_dir(target)}/frameworks/UIKit.framework/"],
-        :rule_name => 'cp_r',
-        :inputs => ["#{ENV['EMSCRIPTEN']}/system/frameworks/UIKit.framework/Resources/"],
+        outputs: ["#{build_dir(target, build_config)}/frameworks/UIKit.framework/"],
+        rule_name: 'cp_r',
+        inputs: ["#{ENV['EMSCRIPTEN']}/system/frameworks/UIKit.framework/Resources/"],
       }
-      resources << "#{build_dir(target)}/frameworks/UIKit.framework/"
+      resources << "#{build_dir(target, build_config)}/frameworks/UIKit.framework/"
 
       builds << {
-        :outputs => [data_path(target)],
-        :rule_name => 'file_packager',
-        :inputs => resources,
+        outputs: [data_path(target, build_config)],
+        rule_name: 'file_packager',
+        inputs: resources,
       }
 
       builds
@@ -186,10 +185,10 @@ RULES
       builds = []
       objects = []
 
-      header_dirs = xcodeproj.main_group.recursive_children.select { |g| g.path && File.extname(g.path) == '.h' }.map{ |g|
-        full_path = File.join((g.parents + [g]).map{ |group| group.path }.select{ |path| path })
+      header_dirs = xcodeproj.main_group.recursive_children.select { |g| g.path && File.extname(g.path) == '.h' }.map do |g|
+        full_path = File.join((g.parents + [g]).map(&:path).select { |path| path })
         File.dirname(full_path)
-      }.to_a.uniq
+      end.to_a.uniq
 
       # build settings
       bs = build_config.build_settings
@@ -198,20 +197,20 @@ RULES
       target_header_dirs = expand(bs['HEADER_SEARCH_PATHS'], :array)
 
       lib_options = lib_dirs.map { |dir| "-L#{dir}" }.join(' ')
-      framework_options = (framework_dirs.map { |f| "-F#{f}" } + REFERENCE_FRAMEWORKS.map{ |f| "-framework #{f}" }).join(' ')
-      header_options = (header_dirs + target_header_dirs).map{ |dir| "-I./#{dir}" }.join(' ')
+      framework_options = (framework_dirs.map { |f| "-F#{f}" } + REFERENCE_FRAMEWORKS.map { |f| "-framework #{f}" }).join(' ')
+      header_options = (header_dirs + target_header_dirs).map { |dir| "-I./#{dir}" }.join(' ')
 
       # build sources
       phase.files_references.each do |file|
-        path = File.join(file.parents.map{ |group| group.path }.select{ |path| path }, file.path)
-        object = File.join(objects_dir(target), path.gsub(/\.[A-Za-z0-9]$/, '.o'))
+        source_path = File.join(file.parents.map(&:path).select { |path| path }, file.path)
+        object = File.join(objects_dir(target, build_config), path.gsub(/\.[A-Za-z0-9]$/, '.o'))
 
         objects << object
 
         settings = file.build_files[0].settings
         # TODO: set default option
-        file_opt = '-s FULL_ES2=1 -O0 -DGL_GLEXT_PROTOTYPES=1 -D__IPHONE_OS_VERSION_MIN_REQUIRED=70000 -D__CC_PLATFORM_IOS=1 -DDEBUG=1 -DCD_DEBUG=0 -DCOCOS2D_DEBUG=0 -DCC_TEXTURE_ATLAS_USE_VAO=0 -Wno-warn-absolute-paths'
-        if settings && settings.has_key?('COMPILER_FLAGS')
+        file_opt = '-s FULL_ES2=1 -O0 -DGL_GLEXT_PROTOTYPES=1 -D__IPHONE_OS_VERSION_MIN_REQUIRED=70000 -D__CC_PLATFORM_IOS=1 -DDEBUG=1 -DCD_DEBUG=0 -DCOCOS2D_DEBUG=0 -DCC_TEXTURE_ATLAS_USE_VAO=0 -Wno-warn-absolute-paths '
+        if settings && settings.key?('COMPILER_FLAGS')
           file_opt += expand(settings['COMPILER_FLAGS'], :array).join(' ')
         end
         file_opt += ' -fobjc-arc' unless file_opt =~ /-fno-objc-arc/
@@ -221,22 +220,22 @@ RULES
         prefix_pch = "#{target.product_name}/Prefix.pch"
 
         builds << {
-          :outputs => [object],
-          :rule_name => 'cc',
+          outputs: [object],
+          rule_name: 'cc',
           # FIXME: fetch pch path from xcodeproj
-          :inputs => [path, prefix_pch],
-          :variables => {
+          inputs: [source_path, prefix_pch],
+          variables: {
             'cflags' => "#{cflags} -include #{prefix_pch}",
-            'source' => path,
+            'source' => source_path,
           }
         }
       end
 
       # link
       builds << {
-        :outputs => [binary_path(target)],
-        :rule_name => 'link',
-        :inputs => objects
+        outputs: [binary_path(target, build_config)],
+        rule_name: 'link',
+        inputs: objects
       }
 
       builds
@@ -250,11 +249,13 @@ RULES
       # FIXME: Implement
     end
 
-    def expand(value, type=nil)
-      if value.kind_of?(Array)
-        value.reject do |v|
+    def expand(value, type = nil)
+      if value.is_a?(Array)
+        value = value.reject do |v|
           v == '$(inherited)'
-        end.map do |v|
+        end
+
+        value.map do |v|
           expand(v)
         end
       else
